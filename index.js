0de72bc52389412f71e12bda03fe753a5bb30ec1
shashwat2003 <info.superpcstduio@gmail.com>
2022-04-19 23:34:29 +0530

    Project Completed

--- index.js
+++ index.js
@@ -9,54 +9,74 @@ let navBarSearch = false;
 
 function imagesLoader() {
     let data = JSON.parse(localStorage.getItem('images_url'))
+    console.log(data)
     let gallery = document.getElementById('gallery')
     gallery.innerHTML = '';
     for (let i = 0; i < data.length; i++) {
-        gallery.innerHTML += `<img src="${data[i]}" alt="Image at ${i}">`
+        gallery.innerHTML += `
+        <div class="image flex">
+        <img src="${data[i]}" alt="Image at ${i}">
+        <div class="hover" onclick="delImage(${i})"><img src="src/del.svg"></div>
+        <div class="hover_mob flex" >Image ${i + 1}<img src="src/del.svg" onclick="delImage(${i})"></div>
+        </div>
+        `
     }
 }
 
 function scroll() {
-    let search = document.getElementsByClassName('search')[0];
-    let nav_search = document.getElementsByClassName('search_nav')[0];
-    if (window.pageYOffset >= search.offsetTop) {
-        if (!navBarSearch) {
-            navBarSearch = true;
-            nav_search.style = 'trasnform: scale(1,1)';
+    if (document.documentElement.clientWidth > 800) {
+        let search = document.getElementsByClassName('search')[0];
+        let nav_search = document.getElementsByClassName('search_nav')[0];
+        if (window.pageYOffset >= search.offsetTop) {
+            if (!navBarSearch) {
+                navBarSearch = true;
+                nav_search.style = 'trasnform: scale(1,1)';
+            }
+        }
+        else {
+            if (navBarSearch) {
+                navBarSearch = false;
+                nav_search.style = 'transform: scale(0,0)';
+            }
         }
     }
-    else {
-        if (navBarSearch) {
-            navBarSearch = false;
-            nav_search.style = 'transform: scale(0,0)';
 
-        }
-
-    }
 }
 
 
 function addImage() {
-    fetch(document.getElementById('input').value, { method: 'head' }).then((res) => {
-        console.log(res.headers.get('content-type').split('/'))
-        if (res.headers.get('content-type').split('/')[0] != 'image') {
+    if (document.getElementById('input').value === '') {
+        alert('URL Cannot Be Empty!')
+    }
+    else {
+        fetch(document.getElementById('input').value, { method: 'head' }).then((res) => {
+            console.log(res.headers.get('content-type').split('/'))
+            if (res.headers.get('content-type').split('/')[0] != 'image') {
+                alert('The requested URL is not an Image!')
+            }
+            else {
+                let data = JSON.parse(localStorage.getItem('images_url'))
+                console.log(data)
+                data.push(document.getElementById('input').value)
+                localStorage.setItem('images_url', JSON.stringify(data))
+                document.getElementById('input').value = ''
+                document.getElementById('input_search').value = ''
+                alert('Image Added!')
+                imagesLoader()
+            }
+        }).catch((err) => {
             alert('The requested URL is not an Image!')
-        }
-        else {
-            let data = JSON.parse(localStorage.getItem('images_url'))
-            console.log(data)
-            data.push(document.getElementById('input').value)
-            localStorage.setItem('images_url', JSON.stringify(data))
-            document.getElementById('input').value = ''
-            imagesLoader()
-        }
-    }).catch((err) => {
-        alert('The requested URL is not an Image!')
-    });
-
+        });
+    }
 
 }
 
+function delImage(index) {
+    let data = JSON.parse(localStorage.getItem('images_url'))
+    data.splice(index, 1);
+    localStorage.setItem('images_url', JSON.stringify(data))
+    imagesLoader()
+}
 function alert(msg) {
     let overlay = document.getElementsByClassName('overlay')[0];
     overlay.innerHTML = msg;
